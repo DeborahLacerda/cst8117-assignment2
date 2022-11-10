c670991b57e5fe166bd3f068b011c836c2f2f87c
Deborah Lacerda <lace0115@algonquinlive.com>
2022-11-10 13:13:19 -0500

    New design folder

--- staff.js
+++ staff.js
@@ -1,14 +1,16 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        var mainContainer = document.getElementById("staffList");
-        for (var i = 0; i < data.length; i++) {
-          var div = document.createElement("div");
-          div.innerHTML = `<img src='https://robohash.org/${data[i].id}'>` + 'Name: ' + data[i].name + "<hr>";
-          mainContainer.appendChild(div);
-        }
-    });
-
-
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var mainContainer = document.getElementById("staffList");
+    for (var i = 0; i < 4; i++) {
+      var div = document.createElement("div");
+      div.innerHTML =
+        `<img src='https://robohash.org/${data[i].id}'>` +
+        "Name: " +
+        data[i].name +
+        "<hr>";
+      mainContainer.appendChild(div);
+    }
+  });
