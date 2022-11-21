68ff1783e8c4a3969b78fd3ca0d969607a39d191
Isabel Santiago <mariaisabelcs@live.com>
2022-11-21 13:56:27 -0500

    add temperature to the main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
-const baseUrl = "https://newsapi.org/v2";
-const apiKey = "2199460b902247be9f80e58b6078abe9";
+const baseUrlNews = "https://newsapi.org/v2";
+const baseUrlWeather = "https://api.openweathermap.org/data/2.5/";
+const apiKeyNews = "2199460b902247be9f80e58b6078abe9";
+const apiKeyWeather = "d228430a7837ca7c487f6914f626939d";
 
-setBannerHeader = (news) => {
+showBannerHeader = (news) => {
   $(document).ready(() => {
-    $(".banner").append(`
+    $("#banner").append(`
       <div class="card mb-3">
           <div class="row g-0">
             <div class="col-md-8">
                 <img src="${
                   news.urlToImage
-                }" class="img-fluid rounded-start" alt="..." />
+                }" class="img-fluid rounded-start" alt="banner-image" />
               </div>
 
               <div class="col-md-4">
@@ -59,18 +61,48 @@ showInformation = (listOfNews, divId) => {
   }
 };
 
+showCurrentDayInformation = (data) => {
+  let currentTemperature = data.main.temp.toFixed(2) + "ºC";
+  let currentCity = data.name;
+  let currentDate = new Date().toDateString();
+  $(document).ready(() => {
+    $(".weather").append(`
+    <span class="d-flex justify-content-end">
+      <h4 class="pe-4">${currentCity} ${currentTemperature}</h4>
+      <h4>${currentDate}</h4>
+    </span>
+    `);
+  });
+};
+
 getNews = (type, country, search, section) => {
   const news = $.ajax({
-    url: `${baseUrl}/${type}`,
+    url: `${baseUrlNews}/${type}`,
     type: "GET",
     data: {
       country: country,
       q: search,
-      apiKey: apiKey,
+      apiKey: apiKeyNews,
     },
   }).done((data) => {
-    if (section == "trending-news") setBannerHeader(data.articles[0]);
+    if (section == "trending-news") showBannerHeader(data.articles[0]);
     showInformation(data.articles, section);
+  });
+
+  return news;
+};
+
+getWeather = (type, cityName) => {
+  const news = $.ajax({
+    url: `${baseUrlWeather}/${type}`,
+    type: "GET",
+    data: {
+      q: cityName,
+      appid: apiKeyWeather,
+      units: "metric",
+    },
+  }).done((data) => {
+    showCurrentDayInformation(data);
   });
 
   return news;
@@ -79,3 +111,4 @@ getNews = (type, country, search, section) => {
 getNews("top-headlines", "ca", "", "trending-news");
 getNews("everything", "", "ottawa", "city-news");
 getNews("everything", "", "sports", "sports-news");
+getWeather("weather", "ottawa");
